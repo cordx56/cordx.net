dda71c18fc0d175a2cc08cbe6a1df646ba0f41ec
Kaoru Chisen <cordx56@cordx.net>
2020-03-17 19:38:37 +0900

    add QRcode

--- pages/meishi/index.vue
+++ pages/meishi/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container flex flex-wrap justify-center items-center mx-auto min-h-screen">
     <div class="w-full md:w-1/3 lg:w-1/4">
-      <img class="mx-auto" src="https://github.com/cordx56.png">
+      <icon />
     </div>
     <div class="w-full md:w-2/3 lg:w-3/4 p-4">
       <profile />
@@ -17,11 +17,13 @@
 </template>
 
 <script>
+import Icon from '~/components/meishi/Icon.vue'
 import Profile from '~/components/meishi/Profile.vue'
 import Links from '~/components/meishi/Links.vue'
 
 export default {
   components: {
+    Icon,
     Profile,
     Links
   }
