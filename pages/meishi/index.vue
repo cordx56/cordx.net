8de48585fec1e96de8c4d08f7c8b074902fbbd40
Kaoru Chisen <cordx56@cordx.net>
2020-03-18 02:21:50 +0900

    modify meishi layout

--- pages/meishi/index.vue
+++ pages/meishi/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container flex flex-wrap justify-center items-center mx-auto min-h-screen">
-    <div class="w-full md:w-1/3 lg:w-1/4">
+    <div class="w-full sm:w-1/3">
       <icon />
     </div>
-    <div class="w-full md:w-2/3 lg:w-3/4 p-4">
+    <div class="w-full sm:w-2/3 p-4">
       <profile />
       <h1 class="title">
         meishi
