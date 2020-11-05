03d06063acbc6d9e24ad93e4f90098ce258231b2
Kaoru Chisen <cordx56@cordx.net>
2020-11-05 19:04:01 +0900

    update meishi profile

--- components/meishi/profile.tsx
+++ components/meishi/profile.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import axios from 'axios'
 
 interface State {
-  ghdata?: any
+  name: string,
+  login: string,
 }
 
 export default class Profile extends React.Component<{}, State> {
   state = {
-    ghdata: undefined
+    name: '',
+    login: '',
   }
   constructor(props) {
     super(props)
@@ -16,26 +18,23 @@ export default class Profile extends React.Component<{}, State> {
   fetchProfile() {
     axios.get('https://api.github.com/users/cordx56')
       .then((response) => {
-        this.setState({ ghdata: response.data })
+        this.setState({
+          name: response.data.name,
+          login: response.data.login,
+        })
       })
       .catch((error) => {
         window.alert(error)
       })
   }
   render() {
-    if (this.state.ghdata) {
-      return (
-        <div>
-          <h1 className="text-4xl font-light">{ this.state.ghdata.name }</h1>
-          <h5 className="text-lg text-gray-700">
-            @{ this.state.ghdata.login }
-          </h5>
-        </div>
-      )
-    } else {
-      return (
-        <div></div>
-      )
-    }
+    return (
+      <div>
+        <h1 className="text-4xl font-light">{ this.state.name }</h1>
+        <h5 className="text-lg text-gray-700">
+          @{ this.state.login }
+        </h5>
+      </div>
+    )
   }
 }
--- pages/meishi.tsx
+++ pages/meishi.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Icon from '../components/meishi/icon'
 import Profile from '../components/meishi/profile'
 import Links from '../components/meishi/links'
@@ -5,13 +6,16 @@ import Links from '../components/meishi/links'
 export default function IndexPage() {
   return(
     <div className="container flex flex-wrap justify-center items-center mx-auto min-h-screen">
-        <div className="w-full sm:w-1/3">
-          <Icon />
-        </div>
-        <div className="w-full sm:w-2/3 p-4">
-          <Profile />
-          <Links />
-        </div>
+      <Head>
+        <title>cordx.net</title>
+      </Head>
+      <div className="w-full sm:w-1/3">
+        <Icon />
       </div>
+      <div className="w-full sm:w-2/3 p-4">
+        <Profile />
+        <Links />
+      </div>
+    </div>
   )
 }
