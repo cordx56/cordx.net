b2181798f2b3c495b582c5876f6232933207ceb5
Kaoru Chisen <cordx56@cordx.net>
2020-11-05 18:54:14 +0900

    implement meishi qrcode flip

--- components/meishi/icon.tsx
+++ components/meishi/icon.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import QRcode from 'qrcode.react'
 
-export default class Icon extends React.Component {
+interface State {
+  qrcode: boolean
+}
+
+export default class Icon extends React.Component<{}, State> {
+  constructor(props) {
+    super(props)
+    this.toggleQR = this.toggleQR.bind(this)
+  }
+  state = {
+    qrcode: false
+  }
+  toggleQR() {
+    this.setState({ qrcode: !this.state.qrcode })
+  }
   render() {
-    return(
-      <img className="mx-auto" src="https://github.com/cordx56.png" />
-    )
+    if (this.state.qrcode) {
+      return (
+        <QRcode className="mx-auto" value="https://cordx.net/" onClick={this.toggleQR} />
+      )
+    } else {
+      return (
+        <img className="mx-auto" src="https://github.com/cordx56.png" onClick={this.toggleQR} />
+      )
+    }
   }
 }
