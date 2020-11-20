047d3f2de75aa66034f2253d8d9f3b8d2b46bce5
Kaoru Chisen <cordx56@cordx.net>
2020-11-20 23:06:02 +0900

    update profile component

--- components/profile.tsx
+++ components/profile.tsx
@@ -3,48 +3,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTools } from '@fortawesome/free-solid-svg-icons'
 import { faIdCard } from '@fortawesome/free-regular-svg-icons'
 
-export default class Profile extends React.Component {
-  render() {
-    return (
-      <div id="profile">
-        <h1 className="text-4xl font-light">Profile</h1>
-        <table className="table-auto w-full">
-          <tbody>
-            <tr className="border-b">
-              <th className="px-2 py-2">
-                <FontAwesomeIcon icon={faTools} className="text-3xl" />
-              </th>
-              <td className="px-2 py-2">
-                <p>Programming lang.</p>
-                <ul className="list-disc pl-6 mb-4">
-                  <li>Python</li>
-                  <li>C++</li>
-                </ul>
-                <p>Environment</p>
-                <ul className="list-disc pl-6">
-                  <li>
-                    <p>Arch Linux</p>
-                    <ul className="list-disc pl-6">
-                      <li>zsh</li>
-                      <li>byobu(tmux)</li>
-                      <li>vim</li>
-                    </ul>
-                  </li>
-                </ul>
-              </td>
-            </tr>
-            <tr className="border-b">
-              <th colSpan={99} className="text-center px-2 py-2">
-                <FontAwesomeIcon icon={faIdCard} className="text-3xl" />
-              </th>
-            </tr>
-            <tr className="border-b">
-              <td className="px-2 py-2">2017-</td>
-              <td className="px-2 py-2">Shibaura Institute of Technology, Information Science and Engineering</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+export default function Profile() {
+  return (
+    <div id="profile">
+      <h1 className="text-4xl font-light">Profile</h1>
+      <table className="table-auto w-full">
+        <tbody>
+          <tr className="border-b">
+            <td colSpan={99} className="px-2 py-2">
+              <h2 className="text-3xl">Affiliation</h2>
+            </td>
+          </tr>
+          <tr className="border-b">
+            <td className="px-2 py-2">2017-</td>
+            <td className="px-2 py-2">Shibaura Institute of Technology, Information Science and Engineering</td>
+          </tr>
+          <tr className="border-b">
+            <td colSpan={99} className="px-2 py-2">
+              <h2 className="text-3xl">Experience</h2>
+            </td>
+          </tr>
+          <tr className="border-b">
+            <td className="px-2 py-2">2020/08</td>
+            <td className="px-2 py-2">
+              <a href="https://cordx56.hatenablog.com/entry/2020/08/31/104330" target="_blank">
+                Hatena Summer Internship
+              </a>
+              <small> (Golang/K8s)</small>
+            </td>
+          </tr>
+          <tr className="border-b">
+            <td className="px-2 py-2">2020/08-</td>
+            <td className="px-2 py-2">
+              Gaiax Internship
+              <small> (Django/React/TypeScript/Vue.js/AWS)</small>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
